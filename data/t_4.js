9af2f4be67c13f2b3e870eceed55e8e94a080f8b
Drawde064 <A01746794@tec.mx>
2024-09-23 18:46:18 -0600

    Add files via upload

---
+++ data/t_4.js
@@ -0,0 +1,1 @@
+var json_t_4 = {"type":"FeatureCollection","name":"t_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1"},"geometry":{"type":"Point","coordinates":[-99.156571622470267,19.424783087488965]}}]}
